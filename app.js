ace3004a56b8ff968967a56c4f8f87ba071559f7
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-23 15:46:55 +0000

    Add index endpoint for heroes

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
+const heroRoutes = require('./api/routes/heroes');
+
+// Logs requests/responses to console
+app.use(morgan('dev'));
+
+// Allows easy access to body of request
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/heroes', heroRoutes);
 
 app.get('/', (req, res) => res.json({ message: 'HELLO WORLD' }));
-
 
 app.listen(1234, () => console.log('Listening on 1234'));
